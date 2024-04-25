0061d18a7b523fada43cf91c503e0fafe8040696
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-25 13:12:00 +0600

    "Added moment library, updated ChatItems component to fetch and display conversations, and modified apiSlice to get token from state"
    Explanation:
    
    * The diff includes the addition of the moment library in package.json and yarn

--- src/components/chat/ChatItems.tsx
+++ src/components/chat/ChatItems.tsx
@@ -1,9 +1,24 @@
+import { useSelector } from "react-redux";
+import { useGetConversationsQuery } from "../../features/conversation/conversationApi";
 import ChatItem from "./ChatItem";
+import RootState from "../../types/types";
 
 export default function ChatItems() {
-  return (
-    <ul>
-      <li>
+  const { user } = useSelector((state: RootState) => state.auth);
+  const { email } = user;
+  const {
+    data: conversations,
+    isLoading,
+    isError,
+  } = useGetConversationsQuery(email);
+  let content = null;
+  if (isLoading) {
+    content = <p>loading ....</p>;
+  } else if (!isLoading && isError) {
+    content = <p>Error occured</p>;
+  } else {
+    content = conversations?.map((conv) => (
+      <li key={conv.id}>
         <ChatItem
           avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
           name="Saad Hasan"
@@ -11,6 +26,7 @@ export default function ChatItems() {
           lastTime="25 minutes"
         />
       </li>
-    </ul>
-  );
+    ));
+  }
+  return <ul>{content}</ul>;
 }
--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,18 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import RootState from "../../app/store"; // Adjust the path as needed
-
-// type Auth = {
-//   token: string | undefined;
-// };
+import RootState from "../../types/types";
 
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API,
     prepareHeaders: async (headers, { getState }) => {
-      // const auth: Auth = (getState() as RootState).auth; // Cast the state to RootState
-      // const token = auth?.token;
-      // if (token) headers.set("Authorization", `Bearer ${token}`);
+      const token: RootState = getState()?.auth?.token;
+      if (token) headers.set("Authorization", `Bearer ${token}`);
       return headers;
     },
   }),
