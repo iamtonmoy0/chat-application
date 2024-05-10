ecb403bdf4752e7d57a67ae56c97de676945660b
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-10 23:45:40 +0600

    "Changed modal button color and updated Navbar logout button styling"
    This commit includes changes to the Modal.jsx component where the button color for the "Send Message" button has been changed from violet to blue. Additionally

--- src/components/chat/Modal.jsx
+++ src/components/chat/Modal.jsx
@@ -44,7 +44,7 @@ export default function Modal({ open, control }) {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
               >
                 Send Message
               </button>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,13 +11,13 @@ export default function Navbar() {
     navigate("/");
   };
   return (
-    <nav className="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
+    <nav className="border-general sticky top-0 z-40 border-b bg-[#008cff] transition-colors">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between h-16 items-center">
           <img className="h-10" src="./assets/lws-logo-dark.svg" />
           <ul>
             <li className="text-white">
-              <button onClick={handleLogout}>Logout</button>
+              <button type="btn" className="bg-[tomato] rounded text-md py-2 px-4" onClick={handleLogout}>Logout</button>
             </li>
           </ul>
         </div>
